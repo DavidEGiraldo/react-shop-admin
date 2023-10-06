fbebf5f99506833e0394451471f1a83b66e55a6c
David Giraldo <degiraldod@gmail.com>
2023-10-06 18:46:03 -0500

    feat: add common, components, main layout, home page, dashboard page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
-import './globals.css';
+import '@/globals.css';
+import Header from '@components/Header';
+import Nav from '@common/Nav';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -11,7 +13,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className='min-h-full'>
+          <Header />
+          <Nav />
+          <main>
+            <div className='max-w-7xl mx-auto py-6 sm:px-6 lg:px-8'>
+              {children}
+            </div>
+          </main>
+        </div>
+      </body>
     </html>
   );
 }
